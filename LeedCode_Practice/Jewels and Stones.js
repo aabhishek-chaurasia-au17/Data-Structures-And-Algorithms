69b5f457999e6b5b928755843d918acacdcbd60a
Abhishek <abhishekc838@gmail.com>
2021-11-15 19:24:03 +0530

    771. Jewels and Stones

---
+++ LeedCode_Practice/Jewels and Stones.js
@@ -0,0 +1,20 @@
+// You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+// Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function(jewels, stones) {
+    let count = 0
+    for(let i = 0; i < jewels.length; i++){
+        for(let j = 0; j < stones.length; j++){
+            if(stones[j] === jewels[i]){
+                count ++
+            }
+        }
+    }
+    return count
+};
